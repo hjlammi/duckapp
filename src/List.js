4c8696fcab9774df36b43cd7b59dd98161130175
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2017-12-12 19:32:20 +0200

    In List add a link to navigate to the top of the page

--- src/List.js
+++ src/List.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import Sightings from './Sightings';
+import ActionHome from 'material-ui/svg-icons/action/home';
 
 class List extends Component {
+  navigateToTop = () => {
+    window.scrollTo(0, 0);
+  }
+
   render() {
     return (
       <div className="sightings">
@@ -10,6 +15,10 @@ class List extends Component {
           Here you can see a list of all the reported duck sightings.
         </p>
         <Sightings />
+        <div onClick={this.navigateToTop} style={{textAlign: "center"}} className="link">
+          <ActionHome/>
+          <p>Navigate to top</p>
+        </div>
       </div>
     );
   }
