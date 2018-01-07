e95e5ca648c06c8c7a3e4a5fbe4eea9071a39900
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2018-01-07 15:00:29 +0200

    Add base path to be used by Links in home component

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,16 @@
 /*The welcome page component that is rendered initially.*/
 
 import React, { Component } from 'react';
-// Let's me navigate inside my app.
+// Enables navigating inside the app.
 import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
+
+    const basePath = "/~hl424546/duckapp";
+    const reportPath = basePath + "/report";
+    const listPath = basePath + "/list";
+
     return (
       <div className="home">
         <h2>What's up, duck!</h2>
@@ -14,8 +19,8 @@ class Home extends Component {
         </p>
         <h3 style={{marginBottom: "5px"}}>Here you can</h3>
           <ul style={{marginTop: "5px", marginLeft: "10px"}}>
-            <li style={{marginBottom: "5px"}}><Link to="/report">report</Link> a duck sighting you made and</li>
-            <li><Link to="/list">browse a list</Link> of all the reported duck sightings.</li>
+            <li style={{marginBottom: "5px"}}><Link to={reportPath}>report</Link> a duck sighting you made and</li>
+            <li><Link to={listPath}>browse a list</Link> of all the reported duck sightings.</li>
           </ul>
       </div>
     );
